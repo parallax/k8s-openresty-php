77ba964cc20e492f0da4b17c59ec032831b2b09a
Lawrence Dudley <lawrence@parall.ax>
2019-09-26 11:27:52 +0100

    first steps for automating deployment

--- build/build-yaml.sh
+++ build/build-yaml.sh
@@ -10,7 +10,9 @@ cp yaml-templates/* yaml-deploy/
 ls -ahls 
 
 # Values
-sed -i '' -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment.yaml
-sed -i '' -e "s#{{ PHP7.1IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.1#g" yaml-deploy/deployment-7.1.yaml
-sed -i '' -e "s#{{ PHP7.2IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.2#g" yaml-deploy/deployment-7.2.yaml
-sed -i '' -e "s#{{ PHP7.3IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.3#g" yaml-deploy/deployment-7.3.yaml
+sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-7.1.yaml
+sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-7.2.yaml
+sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-7.3.yaml
+sed -i -e "s#{{ PHP7.1IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.1#g" yaml-deploy/deployment-7.1.yaml
+sed -i -e "s#{{ PHP7.2IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.2#g" yaml-deploy/deployment-7.2.yaml
+sed -i -e "s#{{ PHP7.3IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.3#g" yaml-deploy/deployment-7.3.yaml
