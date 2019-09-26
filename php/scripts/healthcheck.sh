ad7353627d34c1ecad2283ccfd9d9f371b3d1f03
Lawrence Dudley <lawrence@parall.ax>
2019-09-26 15:47:59 +0100

    rm nginx php health check, add fcgi and healthcheck.sh for testing into php image

---
+++ php/scripts/healthcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check the status of the PHP-FPM Healthcheck:
+
+OUTPUT=`SCRIPT_NAME=/healthz-php \
+SCRIPT_FILENAME=/healthz-php \
+REQUEST_METHOD=GET \
+cgi-fcgi -bind -connect /run/php.sock`
+
+if [[ $OUTPUT == *"PHP-FPM OK"* ]]; then
+	echo "PHP-FPM OK"
+	exit
+else
+	echo "PHP-FPM Error"
+	exit 1
+fi
