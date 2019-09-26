ecbeec991a976142d35fe07f17a65fc1e615e2bf
Lawrence Dudley <lawrence@parall.ax>
2019-09-26 11:24:10 +0100

    first steps for automating deployment

--- build/build-yaml.sh
+++ build/build-yaml.sh
@@ -7,6 +7,8 @@ BUILDNUMBER=$bamboo_buildNumber
 rm -f yaml-deploy/*
 cp yaml-templates/* yaml-deploy/
 
+ls -ahls 
+
 # Values
 sed -i '' -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment.yaml
 sed -i '' -e "s#{{ PHP7.1IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.1#g" yaml-deploy/deployment-7.1.yaml
--- build/deploy-7.1.sh
+++ build/deploy-7.1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Apply to Kubernetes
 kubectl --kubeconfig=$KUBECONFIG apply -f yaml-deploy/namespace.yaml
-kubectl --kubeconfig=$KUBECONFIG apply -f ../yaml-deploy/deployment-7.1.yaml
-kubectl --kubeconfig=$KUBECONFIG apply -f ../yaml-deploy/service-7.1.yaml
+kubectl --kubeconfig=$KUBECONFIG apply -f yaml-deploy/deployment-7.1.yaml
+kubectl --kubeconfig=$KUBECONFIG apply -f yaml-deploy/service-7.1.yaml
