c492ef79f84d68813876835c386ce202d5f15546
Lawrence Dudley <lawrence@parall.ax>
2019-09-26 11:18:02 +0100

    first steps for automating deployment

---
+++ build/deploy-7.1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Apply to Kubernetes
+kubectl --kubeconfig=$KUBECONFIG apply -f ../yaml-deploy/namespace.yaml
+
+#kubectl apply -f ../yaml-deploy/deployment-7.1.yaml
+#kubectl apply -f ../yaml-deploy/service-7.1.yaml
+#kubectl apply -f ../yaml-deploy/deployment-7.2.yaml
+#kubectl apply -f ../yaml-deploy/service-7.2.yaml
+#kubectl apply -f ../yaml-deploy/deployment-7.3.yaml
+#kubectl apply -f ../yaml-deploy/service-7.3.yaml
--- build/deploy-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Apply to Kubernetes
-kubectl apply -f ../yaml-deploy/deployment-7.1.yaml
-kubectl apply -f ../yaml-deploy/service-7.1.yaml
-kubectl apply -f ../yaml-deploy/deployment-7.2.yaml
-kubectl apply -f ../yaml-deploy/service-7.2.yaml
-kubectl apply -f ../yaml-deploy/deployment-7.3.yaml
-kubectl apply -f ../yaml-deploy/service-7.3.yaml
