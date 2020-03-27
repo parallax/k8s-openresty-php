bf9b6114a465bfe982174c9bc9ab23156fd43f6b
Lawrence Dudley <lawrence@parall.ax>
2020-03-27 11:42:55 +0000

    use alpine:3.10 for php7.4

--- build/build-7.4.sh
+++ build/build-7.4.sh
@@ -4,4 +4,4 @@
 BUILDNUMBER=$bamboo_buildNumber
 
 # PHP
-docker build --build-arg PHP_VERSION=7.4 --build-arg ATATUS_VERSION=1.9.0 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.4 -f php/Dockerfile .
+docker build --build-arg PHP_VERSION=7.4 --build-arg ATATUS_VERSION=1.9.0 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.4 -f php/Dockerfile-7.4 .
