914b4214712ca0a2ce4361030b20fff590b5607e
Dan Pegg <dan@parall.ax>
2020-06-10 13:15:25 +0100

    Fix 5.6 test deployment

--- build/build-yaml.sh
+++ build/build-yaml.sh
@@ -8,10 +8,12 @@ rm -f yaml-deploy/*
 cp yaml-templates/* yaml-deploy/
 
 # Values
+sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-5.6.yaml
 sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-7.1.yaml
 sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-7.2.yaml
 sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-7.3.yaml
 sed -i -e "s#{{ OPENRESTYIMAGEHERE }}#prlx/k8s-openresty-php-openresty:build-$BUILDNUMBER#g" yaml-deploy/deployment-7.4.yaml
+sed -i -e "s#{{ PHP5.6IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-5.6-test#g" yaml-deploy/deployment-5.6.yaml
 sed -i -e "s#{{ PHP7.1IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.1-test#g" yaml-deploy/deployment-7.1.yaml
 sed -i -e "s#{{ PHP7.2IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.2-test#g" yaml-deploy/deployment-7.2.yaml
 sed -i -e "s#{{ PHP7.3IMAGEHERE }}#prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.3-test#g" yaml-deploy/deployment-7.3.yaml
