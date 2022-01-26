89c68f0fac665fcfbf1146d54d6df30a342d250f
Dan Pegg <daniel.pegg7849@gmail.com>
2022-01-26 13:22:25 +0000

    Add atatus to PHP 8.1

--- build/build-8.1.sh
+++ build/build-8.1.sh
@@ -4,4 +4,4 @@
 BUILDNUMBER=$bamboo_buildNumber
 
 # PHP
-docker build --build-arg PHP_VERSION=81 --build-arg ATATUS_VERSION=1.12.0 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-8.1 -f php/Dockerfile-8.1 .
+docker build --build-arg PHP_VERSION=81 --build-arg ATATUS_VERSION=1.13.0 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-8.1 -f php/Dockerfile-8.1 .
