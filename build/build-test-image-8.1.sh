b3d946cd8f29a117b4424894e6fd60cfaed5815b
Dan Pegg <daniel.pegg7849@gmail.com>
2022-02-07 13:58:29 +0000

    Build ARM alternatives of PHP 8+ images

--- build/build-test-image-8.1.sh
+++ build/build-test-image-8.1.sh
@@ -2,4 +2,4 @@
 BUILDNUMBER=$bamboo_buildNumber
 
 sed -i -e "s#{{ BUILDNUMBER }}#$BUILDNUMBER#g" test/Dockerfile-8.1
-docker build -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-8.1-test -f test/Dockerfile-8.1 .
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-8.1-test -f test/Dockerfile-8.1 .
