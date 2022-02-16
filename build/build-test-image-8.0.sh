c079eca66226854dbe73444cabc24363f8f0fe29
Dan Pegg <daniel.pegg7849@gmail.com>
2022-02-16 13:39:07 +0000

    Revert "Build ARM alternatives of PHP 8+ images"
    This reverts commit b3d946cd8f29a117b4424894e6fd60cfaed5815b.

--- build/build-test-image-8.0.sh
+++ build/build-test-image-8.0.sh
@@ -2,4 +2,4 @@
 BUILDNUMBER=$bamboo_buildNumber
 
 sed -i -e "s#{{ BUILDNUMBER }}#$BUILDNUMBER#g" test/Dockerfile-8.0
-docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-8.0-test -f test/Dockerfile-8.0 .
+docker build -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-8.0-test -f test/Dockerfile-8.0 .
