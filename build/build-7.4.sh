c88bf01ade8f2e21fcb0e1aed0b0a4fb44d6edce
Dan Pegg <daniel.pegg7849@gmail.com>
2022-05-31 10:37:36 +0100

    Finish up CI

--- build/build-7.4.sh
+++ build/build-7.4.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Used to build and deploy to minikube for local dev
+docker buildx build --push --platform=linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:build-$SHA-php-7.4 -f php/Dockerfile-7.4 .
 
-BUILDNUMBER=$bamboo_buildNumber
-
-# PHP
-docker buildx build --push --platform=linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-7.4 -f php/Dockerfile-7.4 .
+if [ "$BRANCH" == 'master' ]; then
+    docker buildx build --push --platform=linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:release-php-7.4-latest -f php/Dockerfile-7.4 .
+fi
--- build/build-8.1.sh
+++ build/build-8.1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Used to build and deploy to minikube for local dev
+docker buildx build --push --platform=linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:build-$SHA-php-8.1 -f php/Dockerfile-8.1 .
 
-BUILDNUMBER=$bamboo_buildNumber
-
-# PHP
-docker buildx build --push --platform=linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:build-$BUILDNUMBER-php-8.1 -f php/Dockerfile-8.1 .
+if [ "$BRANCH" == 'master' ]; then
+    docker buildx build --push --platform=linux/arm64/v8,linux/amd64 -t prlx/k8s-openresty-php-php:release-php-8.1-latest -f php/Dockerfile-8.1 .
+fi
